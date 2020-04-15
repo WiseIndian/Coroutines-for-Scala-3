95d4c68211a0b14ff6621dc84bda05ba3e1c0f96
simon.lebail-collet <simon.lebail-collet@lilo.org>
2020-04-16 00:14:10 +0200

    Remove If design file. Add some coroutine in the main. Add tests for if and vals

--- library/src/main/scala/Main.scala
+++ library/src/main/scala/Main.scala
@@ -1,8 +1,15 @@
+import coroutines._
+import coroutines.Macros._
 
-// object Main { 
-//   def main(args: Array[String]): Unit = {
-//     //(5+6).debug
-//     // Macros.debug2((5+6)) 
-//   }
+object Main { 
+  def main(args: Array[String]): Unit = {
+    val co = coroutine[Int]({
+      println("hello world")
+      yieldval(2*2)
+      yieldval(3)
+    })
 
-// }
+    (0 until 3) foreach (_ => println(co.continue()))
+  }
+
+}
--- library/src/test/scala/Test1.scala
+++ library/src/test/scala/Test1.scala
@@ -7,12 +7,12 @@ class Test1 {
   @Test def test1(): Unit = {
     val co = coroutine[Int]({
       print("hello world")
-      yieldval(1+1)
       yieldval(2*2)
+      yieldval(3)
     })
 
-    List(Some(2), Some(4), None).foreach { v => 
-      assertEquals(co.continue, v)
+    List(Some(4), Some(3), None).foreach { v => 
+      assertEquals(co.continue(), v)
     }
   }
 
@@ -22,7 +22,7 @@ class Test1 {
     })
 
     List(None).foreach { v => 
-      assertEquals(co.continue, v)
+      assertEquals(co.continue(), v)
     }
   }
 
@@ -31,7 +31,7 @@ class Test1 {
     })
 
     List(None).foreach { v => 
-      assertEquals(co.continue, v)
+      assertEquals(co.continue(), v)
     }
   }
 
@@ -43,7 +43,7 @@ class Test1 {
     })
 
     List(Some(3), None).foreach { v => 
-      assertEquals(co.continue, v)
+      assertEquals(co.continue(), v)
     }
   }
 
@@ -55,176 +55,174 @@ class Test1 {
     })
 
     List(Some(4), None).foreach { v => 
-      assertEquals(co.continue, v)
+      assertEquals(co.continue(), v)
     }
   }
 
 
 
-  @Test def testIfDesign1(): Unit = {
-    /* `co` is supposed to be the translation of:
-    coroutine[Int] {
+
+  @Test def withValsTest3(): Unit = {
+    val co = coroutine[Int]({
+      val x = 3
+      yieldval(x+1)
+      yieldval(x+2)
+    })
+    //for this test to pass I have to take value definitions out of the if scope
+    List(Some(4), Some(5), None).foreach { v => 
+      assertEquals(co.continue(), v)
+    }
+  }
+
+  class B {
+    var y = 0
+  }
+
+  @Test def testIf1(): Unit = {
+    val b: B = new B()
+
+
+    val co = coroutine[Int] ({
       val bVal = b.y 
       if (b.y < 10) {
         val x = 3
-        yieldval 10
+        yieldval(10)
         println(x)
       }
-      yieldval bVal
-    }
-    */
-    val co = new IfCoroutineExample1(new B())
-    assertEquals(co.continue, Some(10))
-    assertEquals(co.continue, Some(new B().y))
-    assertEquals(co.continue, None)
+      yieldval(bVal)
+    })
+
+    assertEquals(Some(10), co.continue())
+    b.y = 11
+    assertEquals(Some(new B().y), co.continue())
+    assertEquals(None, co.continue())
   }
 
-  @Test def testIfDesign2(): Unit = { 
-    val b = new B()
-    val co = new IfCoroutineExample1(b)
-    assertEquals(co.continue, Some(10))
-    b.y = 111
-    assertEquals(co.continue, Some(new B().y))
-    assertEquals(co.continue, None)
+  @Test def testIf2(): Unit = {
+    val b: B = new B()
+
+
+    val co = coroutine[Int] ({
+      val bVal = b.y 
+      if (b.y < 10) {
+        val x = 3
+        yieldval(10)
+        println(x)
+      }
+      yieldval(bVal)
+    })
+
+    assertEquals(co.continue(), Some(10))
+    assertEquals(co.continue(), Some(new B().y))
+    assertEquals(co.continue(), None)
   }
 
-  @Test def testIfDesign3(): Unit = { 
-    val b = new B()
-    b.y = 111
-    val co = new IfCoroutineExample1(b)
-    assertEquals(co.continue, Some(111))
-    assertEquals(co.continue, None)
-  }
- 
+
+
 
   @Test def testIfDesign4(): Unit = {
-    /*
-    coroutine[Int] {
+    
+    val co = coroutine[Int] {
+      val x = 0
       if (x < 10) {
           yieldval(1)
       } else {
           yieldval(2)
       }
     }
-    */
-    val co = new IfCoroutineExample2(x = 0)
-    List(Some(1), None) foreach(assertEquals(_, co.continue))
+    
+    List(Some(1), None) foreach(assertEquals(_, co.continue()))
   }
 
 
 
+  def getCoroutine1(x: Int, y: Int): Coroutine[Int] = 
+    coroutine[Int] {
+      if (x < 10) {
+        if (y < 10) {
+          yieldval(1)
+          yieldval(2)
+        } else {
+          yieldval(3)
+        }
+      } else {
+        yieldval(4)
+      }
+    } 
 
   @Test def testIfDesign5(): Unit = {
-  /*
-coroutine[Int] {
-    
-    if (x < 10) {
-        if (y < 10) {
-            yieldval(1)
-            yieldval(2)
-        } else {
-            yieldval(3)
-        }
-    } else {
-        yieldval(4)
-    }
-} 
-*/
-    val co = new IfCoroutineExample3(x = 0, y = 0)
-    List(Some(1), Some(2), None, None, None) foreach(assertEquals(_, co.continue))
+  
+    val co = getCoroutine1(0,0);
+
+    List(Some(1), Some(2), None, None, None) foreach(assertEquals(_, co.continue()))
   }
 
 
   @Test def testIfDesign6(): Unit = {
-    val co = new IfCoroutineExample3(x = 0, y = 10)
-    List(Some(3), None, None) foreach(assertEquals(_, co.continue))
+    val co = getCoroutine1(x = 0, y = 10)
+    List(Some(3), None, None) foreach(assertEquals(_, co.continue()))
   }
 
 
   @Test def testIfDesign7(): Unit = {
-    val co = new IfCoroutineExample3(x = 10, y = 10)
-    List(Some(4), None, None) foreach(assertEquals(_, co.continue))
+    val co = getCoroutine1(x = 10, y = 10)
+    List(Some(4), None, None) foreach(assertEquals(_, co.continue()))
   }
 
 
   @Test def testIfDesignScope1(): Unit = {
-  // coroutine[Int] {
-  //   val x = 0
-  //   if (x == 0) {
-  //     val x = 1
-  //     yieldval(x)
-  //   }
+    val co = coroutine[Int] {
+      val x = 0
+      if (x == 0) {
+        val x = 1
+        yieldval(x)
+      }
 
-  //   yieldval(x)
-  // }
+      yieldval(x)
+    }
 
 
-    val co = IfCoroutineExample4()
-    List(Some(1), Some(0), None)  foreach(assertEquals(_, co.continue))
+    List(Some(1), Some(0), None)  foreach(assertEquals(_, co.continue()))
+  }
+
+  def getCoroutine2(initialX: Int): Coroutine[Int] = {
+    coroutine[Int] {
+        val x = initialX
+        if (x == 0) {
+            val x = 1
+            yieldval(x)
+            if (x == 1) {
+                val x = 2
+                yieldval(x)
+            }
+            yieldval(x)
+        }
+        yieldval(x)
+    }
   }
 
   @Test def testIfDesignScope2(): Unit = {
-    val co = IfCoroutineExample5(0)
-    List(Some(1), Some(2), Some(1), Some(0), None)
-    .foreach(assertEquals(_, co.continue))
+    val co = getCoroutine2(initialX = 0)
+    List(Some(1), Some(2), Some(1), Some(0), None).foreach(assertEquals(_, co.continue()))
   }
 
   @Test def testIfDesignScope3(): Unit = {
-    val co = IfCoroutineExample5(5)
-    List(Some(5), None)
-    .foreach(assertEquals(_, co.continue))
+    val co = getCoroutine2(initialX = 5)
+    List(Some(5), None).foreach(assertEquals(_, co.continue()))
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
 //TODO recreate if design tests for the case when we'll have automated the creation of coroutine with if in their body.
-
-  // @Test def withValsTest3(): Unit = {
-  //   val co = coroutine[Int]({
-  //     val x = 3
-  //     yieldval(x+1)
-  //     yieldval(x+2)
-  //   })
-  //   //for this test to pass I have to take value definitions out of the if scope
-  //   List(Some(4), Some(5), None).foreach { v => 
-  //     assertEquals(co.continue, v)
-  //   }
-  // }
-
-  // @Test def testIf1(): Unit = {
-  //   val b: B = new B()
-
-
-  //   val co = coroutine[Int] ({
-  //     val bVal = b.y 
-  //     if (b.y < 10) {
-  //       val x = 3
-  //       yieldval(10)
-  //       println(x)
-  //     }
-  //     yieldval(bVal)
-  //   })
-
-  //   assertEquals(co.continue, 10)
-  //   b.y = 11
-  //   assertEquals(co.continue, new B().y)
-  // }
-
-  // @Test def testIf2(): Unit = {
-  //   val b: B = new B()
-
-
-  //   val co = coroutine[Int] ({
-  //     val bVal = b.y 
-  //     if (b.y < 10) {
-  //       val x = 3
-  //       yieldval(10)
-  //       println(x)
-  //     }
-  //     yieldval(bVal)
-  //   })
-
-  //   assertEquals(co.continue, 10)
-  //   assertEquals(co.continue, new B().y)
-  // }
-
 
 }

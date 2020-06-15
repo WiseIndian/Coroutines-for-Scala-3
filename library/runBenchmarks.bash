4056aeb6d7da10cbf9fcee17cf86df5432ae1ffd
simon.lebail-collet <simon.lebail-collet@lilo.org>
2020-06-15 03:38:36 +0200

    remove thread number specification in runBenchmarks.bash

--- library/runBenchmarks.bash
+++ library/runBenchmarks.bash
@@ -1,10 +1,10 @@
 #this will run jmh benchmarks
-sbt 'jmh:run -rf json -rff fibo.json -t 8 .*FibonacciBenchJMH.* -jvmArgs "-Xms1G -Xmx1G"' 2>&1 > fibo.out
+sbt 'jmh:run -rf json -rff fibo.json .*FibonacciBenchJMH.* -jvmArgs "-Xms1G -Xmx1G"' 2>&1 > fibo.out
 echo done with fibo
 
-sbt 'jmh:run -rf json -rff taylor.json -t 8 .*TaylorBenchJMH.* -jvmArgs "-Xms1G -Xmx1G"' 2>&1 > taylor.out
+sbt 'jmh:run -rf json -rff taylor.json  .*TaylorBenchJMH.* -jvmArgs "-Xms1G -Xmx1G"' 2>&1 > taylor.out
 echo done with taylor
 
-sbt 'jmh:run -rf json -rff tree.json -t 8 .*TreeIteratorBenchJMH.* -jvmArgs "-Xms1G -Xmx1G"' 2>&1 > tree.out
+sbt 'jmh:run -rf json -rff tree.json  .*TreeIteratorBenchJMH.* -jvmArgs "-Xms1G -Xmx1G"' 2>&1 > tree.out
 
 echo done with tree
